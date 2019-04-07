2c8378271b1f6f112d1ad0f50452f8007b3fa9da
birbjam <41635757+birbjam@users.noreply.github.com>
2019-04-07 10:26:41 -0700

    Added some misc styling and a breakdown of what needs to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+//Form where you can add an animal.
+//When you add an animal, creates a button.
+//Button is linked to the api which then retrieves the animal that was clicked.
+//Displays 10 GIFs
+//GIFs load as still images
+//Clicking on images makes the GIFs move.
+
 
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=hvJfg6OTKsJp7gfJ3BHvuSNBGjqF4sgf";
 
